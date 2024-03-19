d7e6a6ea55cff6d3ba16eb1d6fe5a66c2a6537b9
AUryeong <jykee7030@gmail.com>
2024-03-19 15:12:43 +0900

    우선순위 큐 EnQueue 구현

--- Assets/Scripts/DataStructure/PriorityQueue.cs
+++ Assets/Scripts/DataStructure/PriorityQueue.cs
@@ -1,4 +1,30 @@
-﻿public class PriorityQueue
+﻿using System.Collections.Generic;
+
+public class PriorityQueue
 {
-    
+    private List<PirorityNodeData> priorityNodes;
+    private int TailIndex => priorityNodes.Count - 1;
+    private int RootIndex => TailIndex / 2;
+
+    public void Enqueue(PirorityNodeData priorityNodeData)
+    {
+        priorityNodes.Add(priorityNodeData);
+        int index = TailIndex;
+
+        while (true)
+        {
+            int parentNode = index / 2;
+            if (priorityNodes[parentNode].pirority >= priorityNodeData.pirority) break;
+
+            priorityNodes[index] = priorityNodes[parentNode];
+            priorityNodes[parentNode] = priorityNodeData;
+            index = parentNode;
+        }
+    }
+
+    public struct PirorityNodeData
+    {
+        public NodeData nodeData;
+        public int pirority;
+    }
 }
